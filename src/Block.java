854cfb18866917f8a18e507090123c302642b6ed
Amr Tarek <amrtarekmo@gmail.com>
2018-04-23 05:16:15 +0200

    Edit Block class

--- src/Block.java
+++ src/Block.java
@@ -1,17 +1,22 @@
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Date;
 
 public class Block{
     String hash , prevHash = "" , data ;
     long timeStamp;
 
-    Block(String prevHash , String data , long timeStamp) throws NoSuchAlgorithmException {
+    Block(String prevHash , String data) throws NoSuchAlgorithmException {
         this.data = data;
-        this.timeStamp = timeStamp;
+        this.timeStamp = new Date().getTime();
         this.prevHash = prevHash;
-        this.hash = generateHash(data + prevHash + Long.toString(timeStamp));
+        this.hash = generateHash();
     }
-    public String generateHash(String message)throws NoSuchAlgorithmException {
+    public String generateHash()throws NoSuchAlgorithmException{
+        String input = data + prevHash + Long.toString(timeStamp);
+        return SHA256(input);
+    }
+    public String SHA256(String message)throws NoSuchAlgorithmException {
         MessageDigest mDigest = MessageDigest.getInstance("SHA256");
         byte[] result = mDigest.digest(message.getBytes());
         StringBuffer sb = new StringBuffer();
