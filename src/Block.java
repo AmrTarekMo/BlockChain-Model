34667cb8e8ef6547c52833a19e2e17cef42bd833
Mostafa Garana <mostafagarana@gmail.com>
2018-04-28 13:27:59 +0200

    Add files via upload

--- src/Block.java
+++ src/Block.java
@@ -1,37 +1,47 @@
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blockchain;
+
 import java.util.Date;
+import blockchain.Utility.*;
+import com.google.gson.*;
 
-public class Block{
-    String hash , prevHash = "" , data ;
-    long timeStamp;
+/**
+ *
+ * @author GOOD
+ */
+public class Block extends Utility
+{
 
-    Block(String prevHash , String data){
+    public String hash, prevHash, data; 
+    private long timeStamp,nonce; //in milliseconds.
+
+    
+    public Block(String data,String prevHash )
+    {
         this.data = data;
-        this.timeStamp = new Date().getTime();
         this.prevHash = prevHash;
-        this.hash = generateHash();
+	this.timeStamp = new Date().getTime();
+        this.hash = calculateHash();
     }
-    public String generateHash(){
-        String input = data + prevHash + Long.toString(timeStamp);
-        return SHA256(input);
+    
+    public String calculateHash()
+    {
+        return applySha256(data+prevHash+nonce+timeStamp);
     }
-    public String SHA256(String message) {
-        try {
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            //Applies sha256 to our input,
-            byte[] hash = digest.digest(message.getBytes("UTF-8"));
-
-            StringBuffer hexString = new StringBuffer(); // This will contain hash as hexidecimal
-            for (int i = 0; i < hash.length; i++) {
-                String hex = Integer.toHexString(0xff & hash[i]);
-                if(hex.length() == 1) hexString.append('0');
-                hexString.append(hex);
-            }
-            return hexString.toString();
-        }
-        catch(Exception e) {
-            throw new RuntimeException(e);
-        }
+    
+    public void mineBlock(int difficulty) 
+    {
+		String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0" 
+		while(!hash.substring( 0, difficulty).equals(target))
+                {
+			nonce ++;
+			hash = calculateHash();
+		}
+		System.out.println("Block Mined!!! : " + hash);
     }
+            
 }
