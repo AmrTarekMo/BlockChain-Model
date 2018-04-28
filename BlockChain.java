3820b35a1096816666afee84d1f6c6656c17d788
Mostafa Garana <mostafagarana@gmail.com>
2018-04-28 13:45:08 +0200

    Add files via upload

---
+++ BlockChain.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blockchain;
+
+import java.util.ArrayList;
+import com.google.gson.*;
+
+public class BlockChain 
+{
+   public ArrayList<Block> chain;
+   public static int difficulty = 6;
+   
+   
+   public BlockChain()
+   {
+       chain=new ArrayList();
+   }
+   public boolean isValid()
+   {
+       String target = new String(new char[5]).replace('\0', '0'); 
+       for(int i=0 ; i<chain.size()-1 ; i++)
+       {
+           Block current=chain.get(i+1), previous=chain.get(i);
+           if(!current.hash.equals(current.calculateHash()) )
+           {
+               	System.out.println("Current Hashes not equal"); 
+                return false;
+           }
+
+           if(!current.prevHash.equals(previous.hash))
+           {
+               	System.out.println("Previous Hashes not equal");
+                return false;
+           }
+           
+           if(!current.hash.substring( 0, difficulty).equals(target))
+           {
+		System.out.println("This block hasn't been mined");
+		return false;
+           }
+       }
+       
+       return true;
+   }
+}
